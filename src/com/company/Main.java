053df32a340ef595d272c07f16c4138f2ccaa9a4
Takayuki Goto <tak.yuki@gmail.com>
2013-08-14 15:00:25 -0500

    cob spec tests passing

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,23 +1,38 @@
 package com.company;
 
 import com.company.responders.*;
-import com.server.Handlers.FileHandler;
 import com.server.Server;
 
 public class Main {
     private static String rootDir = "/Users/takayuki/Coding/java/cob_spec/public\n";
+    private static int defaultPort = 5000;
 
     public static void main(String[] args) {
-        Server server = new Server(5000, rootDir);
+        parseArgs(args);
+        Server server = new Server(defaultPort, rootDir);
         BasicAuth basicAuth = new BasicAuth();
         server.mount("/", new Root());
         server.mount("/form", new PutPost());
         server.mount("/parameters", new ParameterDecode());
         server.mount("/redirect", new Redirect());
         server.mount("/file1", new FileHandler(rootDir));
+        server.mount("/text-file.txt", new FileHandler(rootDir));
         server.mount("/logs", basicAuth);
+        server.mount("/log", basicAuth);
         server.mount("/these", basicAuth);
         server.mount("/requests", basicAuth);
+        server.mount("/image.jpeg", new FileHandler(rootDir));
+        server.mount("/image.gif", new FileHandler(rootDir));
+        server.mount("/image.png", new FileHandler(rootDir));
         server.listen();
+    }
+
+    private static void parseArgs(String[] args) {
+        try {
+            defaultPort = Integer.parseInt(args[1]);
+            rootDir = args[3];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Proper Usage: java -jar <your jar file> -p <port> -d <directory to serve>");
+        }
     }
 }
